6e89c1667b38bfa41a05cb0bc14894ab2e816d2f
GitBaz <kam.amir73@gmail.com>
2017-04-08 10:52:34 +0430

    BackendData updated

--- library/src/main/java/ir/myandroidapp/library/backend/BackendData.java
+++ library/src/main/java/ir/myandroidapp/library/backend/BackendData.java
@@ -1,8 +1,10 @@
 package ir.myandroidapp.library.backend;
 
 import com.backtory.java.internal.BacktoryCallBack;
+import com.backtory.java.internal.BacktoryFile;
 import com.backtory.java.internal.BacktoryObject;
 
+import java.io.File;
 import java.util.List;
 
 /**
@@ -11,22 +13,31 @@ import java.util.List;
 
 public class BackendData {
 
-    public BackendData(){
+    String table = "";
+
+    public BackendData(String tbl){
+        table=tbl;
     }
 
-    public void getItem(String table, String id, BacktoryCallBack<BacktoryObject> callBack) {
+    public void getById(String id, BacktoryCallBack<BacktoryObject> callBack) {
         BacktoryObject.GetQuery(table).getInBackground(id, callBack);
     }
 
-    public void getItems(String table,String where,String is,BacktoryCallBack<List<BacktoryObject>> callBack){
+    public void getMathes(String where,String is,BacktoryCallBack<List<BacktoryObject>> callBack){
         BacktoryObject.GetQuery(table).whereMatches(where,is).findInBackground(callBack);
     }
 
-    public void search(String table,String key,BacktoryCallBack<List<BacktoryObject>> callBack){
-        BacktoryObject.GetQuery(table).whereExists(key).findInBackground(callBack);
+    public void getContains(String where,String is,BacktoryCallBack<List<BacktoryObject>> callBack){
+        BacktoryObject.GetQuery(table).whereContains(where,is).findInBackground(callBack);
     }
 
+    public void getAll(BacktoryCallBack<List<BacktoryObject>> callBack){
+        BacktoryObject.GetQuery(table).findInBackground(callBack);
+    }
 
+    public void uploadImage(String path){
+        new BacktoryFile().beginUpload(new File(path));
+    }
 
 
 }
